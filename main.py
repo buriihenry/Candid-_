721a6dd86553aca39a8157a12a9e54f69f5dcca8
buriihenry <buriihenry@gmail.com>
2023-01-06 09:47:21 +0300

    FastAPI

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Data": "Testing"}
-@app.get("/about")
-def about():
-    return {"Data": "About"}    
+inventory = {
+    1 :{
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    }
+}
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None, description="The ID of the Item you would like to view")):
+    return inventory[item_id]
+@app.get("/get-by-name")
+def get_item(*, name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id] ["name"] == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}        
