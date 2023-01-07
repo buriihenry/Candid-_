72124c624f2c3e68b8d84bdd728af3e763d2f0f2
buriihenry <buriihenry@gmail.com>
2023-01-07 12:45:57 +0300

    Update #FastAPI

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ class Item(BaseModel):
     price: float
     brand: Optional[str] = None
 
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
 
 inventory = {}
 
@@ -28,4 +33,19 @@ def create_item(item_id: int, item: Item):
     if item_id in inventory:
         return {"Error": "Item ID already exists."}
     inventory[item_id] = item
-    return inventory[item_id]    
+    return inventory[item_id]
+
+@app.put("/update-itme/{item_id}")
+def update_item(item_id: int, item:UpdateItem):
+    if item_id not in inventory:
+        return {"Error": "Item ID does not exists."}
+
+    if item.name != None:
+        inventory[item_id].name = item.name
+    if item.price != None:
+        inventory[item_id].price = item.price
+    if item.brand != None:
+        inventory[item_id].brand = item.brand        
+
+    return inventory[item_id]
+            
