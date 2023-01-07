5c7598f40df6b84865133e9f42c5e0f5622424eb
buriihenry <buriihenry@gmail.com>
2023-01-07 10:03:00 +0300

    FastAPI

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ inventory = {
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(None, description="The ID of the Item you would like to view")):
     return inventory[item_id]
-@app.get("/get-by-name")
-def get_item(*, name: Optional[str] = None, test: int):
+@app.get("/get-by-name/{item_id}")
+def get_item(*, item_id: int, name: Optional[str] = None, test: int):
     for item_id in inventory:
         if inventory[item_id] ["name"] == name:
             return inventory[item_id]
