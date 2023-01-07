1ae4571c4fc66f40315c37245701469928cfad20
buriihenry <buriihenry@gmail.com>
2023-01-07 10:49:45 +0300

    Post Method #API

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-inventory = {
-    1 :{
-        "name": "Milk",
-        "price": 3.99,
-        "brand": "Regular"
-    }
-}
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+
+inventory = {}
+
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(None, description="The ID of the Item you would like to view")):
     return inventory[item_id]
-@app.get("/get-by-name/{item_id}")
-def get_item(*, item_id: int, name: Optional[str] = None, test: int):
+
+@app.get("/get-by-name")
+def get_item(name: str = Query(None, title= "Name", description = "Name of item.")):
     for item_id in inventory:
-        if inventory[item_id] ["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
-    return {"Data": "Not found"}        
+    return {"Data": "Not found"}      
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists."}
+    inventory[item_id] = item
+    return inventory[item_id]    
